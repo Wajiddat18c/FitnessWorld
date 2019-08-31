e2a93434345ab7b8419701d0b457b62d8887611c
Wajiddat18c <wajid0003@stud.kea.dk>
2019-09-01 01:59:17 +0200

    Added comments

--- FitnessWorld/src/Main.java
+++ FitnessWorld/src/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Member> people = new ArrayList<>();
+        List<Employee> employeeList = new ArrayList<>();
+
+
         people.add(new Member("Ali", "090689-5010", false));
         people.add(new Member("Wajid", "218450-2349", true));
         people.add(new Member("jarl", "345678-1234", true));
         people.add(new Member("Alex", "871023-9999", false));
         people.add(new Member("Jonas", "741023-6541", true));
 
-
-        List<Employee> employeeList = new ArrayList<>();
         employeeList.add(new Instructor("Kim", "050649-8520", 120));
         employeeList.add(new Administration("Bi", "784325-2015", 2));
         employeeList.add(new Instructor("Bo", "648720-0022", 10));
         employeeList.add(new Administration("John", "367405-4564", 5));
-
 
 
         printNameAndCpr((ArrayList<Member>) people, (ArrayList<Employee>) employeeList);
@@ -27,7 +27,8 @@ public class Main {
 
     }
 
-    public static void printNameAndCpr(ArrayList<Member>people, ArrayList<Employee>employeeList) {
+    // Denne metode printer alle members og employess navn og cpr.
+    public static void printNameAndCpr(ArrayList<Member> people, ArrayList<Employee> employeeList) {
 
 
         System.out.println("Members Navn And Cpr");
@@ -36,17 +37,18 @@ public class Main {
         for (Member p :
                 people) {
 
-                System.out.printf("%-15s%-12s\n", p.getNavn(), p.getCpr());
+            System.out.printf("%-15s%-12s\n", p.getNavn(), p.getCpr());
         }
-        for (Employee e:
-             employeeList) {
+        for (Employee e :
+                employeeList) {
             System.out.printf("%-15s%-12s\n", e.getNavn(), e.getCpr());
 
         }
         end();
     }
 
-    public static void fitnessMembers(ArrayList<Member>people) {
+    //Denne metode printer alle members info ud.
+    public static void fitnessMembers(ArrayList<Member> people) {
 
         System.out.println("Fitness Members");
         System.out.printf("%-15s%-12s%-12s%-12s\n", "Navn", "Cpr", "Member type", "Fee");
@@ -59,7 +61,8 @@ public class Main {
         end();
     }
 
-    public static void printEmployee(ArrayList<Employee>employeeList) {
+    // Denne metode printer alle employyes info ud.
+    public static void printEmployee(ArrayList<Employee> employeeList) {
         System.out.println("Fitness Employees");
         System.out.printf("%-15s%-12s%-12s%-12s%-12s\n", "Navn", "Cpr", "Hours", "Salary", "Vacation");
         start();
@@ -75,6 +78,7 @@ public class Main {
         end();
     }
 
+    // Denne metode laver *
     public static void start() {
 
         for (int i = 0; i < 75; i++) {
@@ -83,6 +87,7 @@ public class Main {
         System.out.println("");
     }
 
+    //Demme metode laver =
     public static void end() {
         for (int i = 0; i < 75; i++) {
             System.out.print("=");
