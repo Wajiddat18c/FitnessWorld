327cc5a723e33ac5093acb5a00c0a259ca3a9f01
Wajiddat18c <wajid0003@stud.kea.dk>
2019-09-01 18:31:51 +0200

    txt can now be read

--- FitnessWorld/src/FileHandling.java
+++ FitnessWorld/src/FileHandling.java
@@ -1,4 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FileHandling {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("FitnessWorld/navneliste/persons.txt"));
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNextLine()){
+            list.add(s.nextLine());
+            System.out.println(list);
+        }
+        s.close();
+    }
 
 
 }
